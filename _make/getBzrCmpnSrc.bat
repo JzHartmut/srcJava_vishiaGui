db194737dfdd4d0763ff7063d7dd1de60ed883f1
Hartmut Schorrig <hartmut@vishia.org>
2012-02-22 00:00:00 +0100

    added _make/bzrGetVersion from the other components

---
+++ _make/getBzrCmpnSrc.bat
@@ -0,0 +1,4 @@
+REM This sources are able to compile only with the correct sources of other components. get it.
+
+bzrGetVersion srcJava_Zbnf 103 ../../srcJava_Zbnf
+bzrGetVersion srcJava_vishiaRun 27 ../../srcJava_vishiaRun
